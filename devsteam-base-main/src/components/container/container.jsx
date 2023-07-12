16101a05f2dc8e994e389bda5e8255a5d49b97eb
len-on <lenon.assalin@gmail.com>
2023-07-12 16:14:31 -0300

    criado salecard

--- devsteam-base-main/src/components/container/container.jsx
+++ devsteam-base-main/src/components/container/container.jsx
@@ -1,5 +1,5 @@
 import styles from './container.module.css'
-export default function Container({children}) {
+export default function Container({ children }) {
     return (
         <div className={styles.container}>
             {children}
